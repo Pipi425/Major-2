b3fa7116b60b0404d6ad927fe36da6fa36e38e65
Shanming Liu <shanmingliu3500@gmail.com>
2026-05-25 10:41:06 -0400

    Add files via upload

---
+++ Maps/Eldermoor/city.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="city" tilewidth="16" tileheight="16" tilecount="48" columns="6">
+ <image source="../Cute_Fantasy_Free/Outdoor decoration/House_1_Wood_Base_Blue.png" width="96" height="128"/>
+</tileset>
